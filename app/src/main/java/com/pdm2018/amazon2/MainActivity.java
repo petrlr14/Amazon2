92f9a6583f6c6d0dfb42f8cfdd0479dd62063183
petrlr14 <00057616@uca.edu.sv>
2018-06-19 08:14:14 -0600

    first commit, se agregaron las librerias a usar sujetas a cambio

---
+++ app/src/main/java/com/pdm2018/amazon2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pdm2018.amazon2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
