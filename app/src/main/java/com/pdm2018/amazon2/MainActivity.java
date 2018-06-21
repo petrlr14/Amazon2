f98e261b2befa4673fa98bd9824d93cb211c3e42
CarlosDubon <00058016@uca.edu.sv>
2018-06-21 08:32:51 -0600

    OBSERVADORES DE CONEXION Y DETECCION DE SEÑAL

--- app/src/main/java/com/pdm2018/amazon2/MainActivity.java
+++ app/src/main/java/com/pdm2018/amazon2/MainActivity.java
@@ -1,13 +1,54 @@
 package com.pdm2018.amazon2;
 
+import android.net.ConnectivityManager;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.github.pwittchen.reactivenetwork.library.rx2.Connectivity;
+import com.github.pwittchen.reactivenetwork.library.rx2.ConnectivityPredicate;
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
+    ConstraintLayout containerL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        containerL = findViewById(R.id.container);
+        ReactiveNetwork.observeNetworkConnectivity(this)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Connectivity>() {
+                    @Override public void accept(final Connectivity connectivity) {
+                        // do something with connectivity
+                        // you can call connectivity.getState();
+                        // connectivity.getType(); or connectivity.toString();
+                        Log.d("Conectivity",connectivity.toString());
+
+                    }
+                });
+
+        ReactiveNetwork.observeInternetConnectivity()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Boolean>() {
+                    @Override public void accept(Boolean isConnectedToInternet) {
+                        // do something with isConnectedToInternet value
+                        if(isConnectedToInternet) {
+                            Snackbar.make(containerL, "INTERNET", Snackbar.LENGTH_INDEFINITE).show();
+                        }else{
+                            Snackbar.make(containerL, "SIN INTERNET - CON CONEXION", Snackbar.LENGTH_INDEFINITE).show();
+                        }
+
+                    }
+                });
+
     }
 }
